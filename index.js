576e84877eb56531673d5418eb73d1a406270b6f
vickyiitbhu <vicky.bijarniya.civ20@itbhu.ac.in>
2024-09-09 23:17:58 +0530

    changes for deployement-6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const mongoDB=require('../db')
+const mongoDB=require('./db')
 
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","*");
